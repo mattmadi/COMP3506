5d071fc8a7cdb136d2c19db7cc8eda750df1db5b
Haziq <hbnordin2@gmail.com>
2016-07-31 18:11:48 +1000

    Answer to problem set 1
    Solutions to the first problem set

---
+++ set1.py
@@ -0,0 +1,65 @@
+#Find out if n is even
+def q1(n):
+    div = n//2
+    remainder = n-div*2
+    if(remainder!=0):
+        return False
+    return True
+
+#Find out if n mod m
+def q2(n, m):
+    div = n//m
+    remainder = n-div*m
+    return remainder
+
+"""
+This works because after you start checking integers
+that are greater than sqrt(n), the only integers that
+m>sqrt(n) can be multiplied with to equal n are less than
+sqrt(n), which have already been checked.
+"""
+#Find out if n is prime
+def q3(n):
+    x = 2
+    stopper = x*x
+    while(stopper<n):
+        if(q2(n, x) == 0):
+            return true
+        x += 1
+        stopper = x*x
+    return false
+
+#Find out m^n where n=2^x where x is an integer
+def q4(n, m):
+    if(n==0):
+        return 1
+    if(n==1):
+        return m
+    x = q4(n//2, m)
+    return x*x
+
+"""
+
+"""
+#Find out if a there is an element in a sorted list that is an element
+#of an unsorted list
+
+def binary_search(element, array):
+    if(len(array)==1):
+        if(array[0] == element):
+            return True
+        return False
+    left=0
+    right=len(array)
+    middle=right//2
+    if(array[middle]==element):
+        return True
+    elif(array[middle]>element):
+        return binary_search(element, array[0:middle])
+    else:
+        return binary_search(element, array[middle: right])
+
+def q5(sorted_list, unsorted_list):
+    if(len(unsorted_list)==1):
+        return binary_search(unsorted_list[0], sorted_list)
+    return(q5(sorted_list, unsorted_list[1:]) or q5(sorted_list, [unsorted_list[0]]))
